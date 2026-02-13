f7a588bec0577b1c34612a41e1a0db4a00accadd
Kwistopha <kjrodriguez899@gmail.com>
2026-02-12 23:59:37 -0700

    Refactor goYes and goNo functions; add button shrink effect

--- script.js
+++ script.js
@@ -5,3 +5,21 @@ function goYes() {
 function goNo() {
   window.location.href = "no.html";
 }
+
+const noBtn = document.getElementById("noBtn");
+
+noBtn.addEventListener("click", shrinkButton);
+
+function shrinkButton() {
+  const style = window.getComputedStyle(noBtn);
+  const width = parseFloat(style.width);
+  const height = parseFloat(style.height);
+
+
+  const newWidth = width * 0.9;
+  const newHeight = height * 0.9;
+
+  noBtn.style.width = newWidth + "px";
+  noBtn.style.height = newHeight + "px";
+}
+
